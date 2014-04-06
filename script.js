b2839f7400d0174e092338610131fb7e1a8497eb
BdNp <brad.naprixas@gmail.com>
2014-04-05 23:02:30 -0400

    Removing unused elements

--- script.js
+++ script.js
@@ -1,63 +1,51 @@
-	var store;
+// Main Webviewer function
 
-	// Main Webviewer function
-
-	function loadIntoFrame(obj, framestate) {
-		if (framestate != 'url') {
-			if (framestate == 'reload') {
-				$('#webview iframe').fadeOut(150)
-				var index = $(obj).attr('nav');	
-			} else {
-				var index = $(obj).attr('post');	
-			}
-			nextPrevCycle(parseInt(index) - 1, parseInt(index) + 1);
+function loadIntoFrame(obj, framestate) {
+	if (framestate != 'url') {
+		if (framestate == 'reload') {
+			$('#webview iframe').fadeOut(150)
+			var index = $(obj).attr('nav');	
 		} else {
-			nextPrevCycle(0,1);
+			var index = $(obj).attr('post');	
 		}
-
-		var source = ((typeof obj) == 'object') ? $(obj).attr('rel') : url; //('http://' + url);
-
-		if (framestate != "url") {
-			$('#webview .share-this > div').html($(obj).closest('.hentry').find('.share-links').html());
-		} else {
-			$('#webview .share-this > div').html($('.hentry:first').find('.share-links').html());
-			// $('#webview .share-facebook').attr('href', ('/go-well/?url=' + url + '&share=facebook'));
-			// $('#webview .share-twitter').attr('href', ('/go-well/?url=' + url + '&share=twitter'));
-		}
-		
-		if (framestate != 'reload') $('#webview').fadeIn();
-		$('#webview iframe').html();
-		$('#webview iframe').attr('src', '');
-		$('#webview iframe').attr('src', source);
-		$('#webview iframe').load(function(){ $('#webview iframe').fadeIn(150) } );
-		
-		//hide feedback button
-		$('#usrFeedbackButton').hide();
-		$('body').css('overflow', 'hidden');
-		$('body').css('height', '100%');
-		
-		window.history.pushState('', '', '/go-well/?url=' + source);
-		// forcePageview('http://www.veria.com/go-well/?url=' + url);
+		nextPrevCycle(parseInt(index) - 1, parseInt(index) + 1);
+	} else {
+		nextPrevCycle(0,1);
 	}
 
-	// Left/Right arrows cycle through website posts
-	function nextPrevCycle(prev, next) {
-		if(prev == 0) 
-			 $('.previous.frame-switch').fadeOut();
-		else $('.previous.frame-switch').fadeIn();
-		if($('a[post=' + next + ']').attr('rel') == null) 
-			 $('.next.frame-switch').fadeOut();
-		else $('.next.frame-switch').fadeIn();
-		console.log('next: ' + $('a[post=' + next + ']').attr('rel'));
-		$('.next.frame-switch').attr('rel', $('a[post=' + next + ']').attr('rel'));
-		$('.previous.frame-switch').attr('rel', $('a[post=' + prev + ']').attr('rel'));
-		$('.next.frame-switch').find('h3').text($('a[post=' + next + ']').find('.post-title').text());
-		$('.previous.frame-switch').find('h3').text($('a[post=' + prev + ']').find('.post-title').text());
-		$('.next.frame-switch').attr('nav', next );
-		$('.previous.frame-switch').attr('nav', prev );		
-	}
+	var source = ((typeof obj) == 'object') ? $(obj).attr('rel') : url; 
 
+	if (framestate != 'reload') $('#webview').fadeIn();
+	$('#webview iframe').html();
+	$('#webview iframe').attr('src', '');
+	$('#webview iframe').attr('src', source);
+	$('#webview iframe').load(function(){ $('#webview iframe').fadeIn(150) } );
 	
+	//hide feedback button
+	$('#usrFeedbackButton').hide();
+	$('body').css('overflow', 'hidden');
+	$('body').css('height', '100%');
+	
+}
+
+// Left/Right arrows cycle through website posts
+function nextPrevCycle(prev, next) {
+	if(prev == 0) 
+		 $('.previous.frame-switch').fadeOut();
+	else $('.previous.frame-switch').fadeIn();
+	if($('a[post=' + next + ']').attr('rel') == null) 
+		 $('.next.frame-switch').fadeOut();
+	else $('.next.frame-switch').fadeIn();
+	console.log('next: ' + $('a[post=' + next + ']').attr('rel'));
+	$('.next.frame-switch').attr('rel', $('a[post=' + next + ']').attr('rel'));
+	$('.previous.frame-switch').attr('rel', $('a[post=' + prev + ']').attr('rel'));
+	$('.next.frame-switch').find('h3').text($('a[post=' + next + ']').find('.post-title').text());
+	$('.previous.frame-switch').find('h3').text($('a[post=' + prev + ']').find('.post-title').text());
+	$('.next.frame-switch').attr('nav', next );
+	$('.previous.frame-switch').attr('nav', prev );		
+}
+
+jQuery(document).ready( function() {
 	// If webviewer's link is manually entered into the domain bar
 	// or if a view is shared, the link takes you straight to the
 	// frame view mode.
